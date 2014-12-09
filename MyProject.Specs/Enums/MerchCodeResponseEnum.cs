ce76abc683d5c0892340f7a892620d980c8a7457
ninjabadgerpirate <ninjabadgerpirate@gmail.com>
2014-12-09 16:46:12 +0200

    Files not checked in last time (eish Git!)

---
+++ MyProject.Specs/Enums/MerchCodeResponseEnum.cs
@@ -0,0 +1,18 @@
+﻿namespace MyProject.Specs.Enums
+{
+    public class MerchCodeResponseEnum
+    {
+        /// <summary>
+        /// Possible ENUM Responses when validating MerchCodes
+        /// </summary>
+        public enum ValidationResponses
+        {
+            Exception = 0,
+            NoMerchCode = 1,
+            InvalidMerchCode = 2,
+            InactiveMerchCode =3,
+            MerchCodeDoesNotExist = 4,
+            Valid = 5
+        }
+    }
+}
---
+++ MyProject.Specs/Enums/OfficeResponseEnum.cs
@@ -0,0 +1,17 @@
+﻿namespace MyProject.Specs.Enums
+{
+    public class OfficeResponseEnum
+    {
+        /// <summary>
+        /// Possible ENUM Responses when validating OfficeCodes
+        /// </summary>
+        public enum ValidationResponses
+        {
+            Exception = 0,
+            InvalidOfficeCode = 1,
+            InactiveOfficeCode = 2,
+            OfficeCodeDoesNotExist = 3,
+            Valid = 4
+        }
+    }
+}
